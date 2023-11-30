fe01dfad1464b01ac9a3880f6bfc9bd8cad3bf94
marcus-silveira <marcusvinicius.767.mv@gmail.com>
2023-11-30 11:01:47 -0300

    criado rota para criar produtos

---
+++ src/products/products.service.ts
@@ -0,0 +1,34 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createProductDto: CreateProductDto) {
+    return this.prismaService.product.create({
+      data: {
+        ...createProductDto,
+        quantity: 0,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
